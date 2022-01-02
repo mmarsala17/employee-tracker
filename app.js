4a38181826b6a1fbfd19c3953f2b6d5a80c20513
Mark Marsala <mmarsala76@gmail.com>
2022-01-01 23:29:02 -0500

    app.js added more updates

--- app.js
+++ app.js
@@ -10,13 +10,32 @@ const startMenuQuestion = [
         message: 'What are you looking to do?',
         choices: [
             "Show all Roles",
+            "Add Role",
             "Show Departments",
             "Add Department",
-            "Show Employees"
+            "Show Employees",
+            "Add Employee"
         ]
     }
 ]
 
+const addRoleQuestion = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title fo the new role?'
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary of the new role?'
+    },
+    {
+        type: 'input',
+        name: 'department',
+        message: 'What department is the new role in? use the number id!'
+    }
+]
 const addDepartmentQuestion = [
     {
         type: 'input',
@@ -24,6 +43,36 @@ const addDepartmentQuestion = [
         message: 'What Department do you need to add?'
     }
 ]
+
+const addEmployeeQuestions = [
+    {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the first name of the employee?'
+    },
+    {
+        type: 'input',
+        name: 'last_name',
+        message: 'What is the last name of the employee?'
+    },
+    {
+        type: 'input',
+        name: 'manager_id',
+        message: 'Who is the manager for the employee? use the number ID!'
+    }
+]
+
+const addRole = async() => {
+    const result = await inquirer.prompt(addRoleQuestions)
+    const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?,?,?)';
+    const params = [result.title, result.salary, result.department,];
+
+    db.query(sql, params,function (err, results) {
+        console.log("");
+        console.table(results);
+    });
+    startMenu();
+}
 
 const addDepartment = async() => {
     const result = await inquirer.prompt(addDepartmentQuestion)
@@ -33,6 +82,18 @@ const addDepartment = async() => {
     db.query(sql, params, function (err, results) {
         console.log("");
         console.table(results);
+    });
+    startMenu();
+}
+
+const addEmployee = async() => {
+    const result = await inquirer.prompt(addEmployeeQuestions)
+    const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)';
+    const params = [result.first_name, result.last_name, result.role_id, result.Manager_id];
+
+    db.query(sql, params, function (err, results) {
+        console.log("");
+        console.table(resluts);
     });
     startMenu();
 }
@@ -48,6 +109,14 @@ const startMenu = async() => {
                 startMenu();
                 break;
 
+                case "Add Role":
+                    db.query('SELECT * FROM department', function (err, results) {
+                        console.log("");
+                        console.table(results);
+                    });
+                    addRole();
+                    break;
+
                 case "Show Departments":
                     db.query('SELCET * FROM department', function (err, results){
                         console.log("");
@@ -59,7 +128,7 @@ const startMenu = async() => {
                     case "Add Department":
                         addDepartment();
                         break;
-                        
+
                     case "Show Employees":
                         db.query('SELECT employee.*, role.title AS role_title FROM employee LEFT JOIN role ON employee.role_id = role.id', function (err, results) {
                             console.log("");
@@ -67,7 +136,19 @@ const startMenu = async() => {
                         });
                         startMenu();
                         break;
-        }
+
+                        case "Add an Employee":
+                            db.query('SELECT role.*, department.name AS department_name FROM role LEFT JOIN department ON role.department_id = department.id', function (err, results) {
+                                console.log("");
+                                console.table(results);
+                            });
+                            db.query('SELECT employee.*, role.title AS role_title FROM employee LEFT JOIN role ON employee.role_id = role.id', function (err, results) {
+                                console.log("");
+                                console.table(results);
+                            });
+                            addEmployee();
+                            break;
+                        }
     });
 }
 
